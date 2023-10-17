5357eb6d7c27fa262b289b8d45bb87cabb7faccd
Dianliang233 <dianliang233@gmail.com>
2023-10-17 20:22:41 +0800

    Add zh-hans translation

--- MigrateUserAccount.i18n.alias.php
+++ MigrateUserAccount.i18n.alias.php
@@ -3,5 +3,9 @@
 $specialPageAliases = [];
 
 $specialPageAliases['en'] = [
-	'MigrateUserAccount' => [ 'MigrateUserAccount', 'Migrate User Account' ],
+	'MigrateUserAccount' => ['MigrateUserAccount', 'Migrate User Account'],
+];
+
+$specialPageAliases['zh-hans'] = [
+	'MigrateUserAccount' => ['迁移用户账号'],
 ];
