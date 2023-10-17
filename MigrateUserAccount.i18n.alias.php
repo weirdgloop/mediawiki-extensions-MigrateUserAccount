f6d4baef0c68fa11592adb840ab5312108d1adf3
Jayden Bailey <jaydenkieran@gmail.com>
2023-10-17 16:46:35 +0100

    Fix formatting issues with zh-hans translation

--- MigrateUserAccount.i18n.alias.php
+++ MigrateUserAccount.i18n.alias.php
@@ -3,9 +3,9 @@
 $specialPageAliases = [];
 
 $specialPageAliases['en'] = [
-	'MigrateUserAccount' => ['MigrateUserAccount', 'Migrate User Account'],
+	'MigrateUserAccount' => [ 'MigrateUserAccount', 'Migrate User Account' ],
 ];
 
 $specialPageAliases['zh-hans'] = [
-	'MigrateUserAccount' => ['迁移用户账号'],
+	'MigrateUserAccount' => [ '迁移用户账号' ],
 ];
