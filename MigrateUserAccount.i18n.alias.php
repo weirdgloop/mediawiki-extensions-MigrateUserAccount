d21a18fe6539562542fefd27fb20da211b000a0e
Jayden Bailey <jaydenkieran@gmail.com>
2023-10-24 18:51:10 +0100

    Add lots of i18n files courtesy of MCW editors

--- MigrateUserAccount.i18n.alias.php
+++ MigrateUserAccount.i18n.alias.php
@@ -6,10 +6,38 @@ $specialPageAliases['en'] = [
 	'MigrateUserAccount' => [ 'MigrateUserAccount', 'Migrate User Account' ],
 ];
 
+$specialPageAliases['pt-br'] = [
+	'MigrateUserAccount' => [ 'Migrarcontadeusuário' ],
+];
+
 $specialPageAliases['zh-hans'] = [
 	'MigrateUserAccount' => [ '迁移用户账号' ],
 ];
 
 $specialPageAliases['uk'] = [
 	'MigrateUserAccount' => [ 'Перенестисвійакаунт' ],
+];
+
+$specialPageAliases['de'] = [
+	'MigrateUserAccount' => [ 'Benutzerkontomigrieren' ]
+];
+
+$specialPageAliases['fr'] = [
+	'MigrateUserAccount' => [ 'Migreruncompteutilisateur' ]
+];
+
+$specialPageAliases['es'] = [
+	'MigrateUserAccount' => [ 'Migrarcuentadeusuario' ]
+];
+
+$specialPageAliases['nl'] = [
+	'MigrateUserAccount' => [ 'Gebruikersaccountmigreren' ]
+];
+
+$specialPageAliases['ru'] = [
+	'MigrateUserAccount' => [ 'Перенестиучётнуюзаписьучастника' ]
+];
+
+$specialPageAliases['th'] = [
+	'MigrateUserAccount' => [ 'โยกย้ายบัญชีผู้ใช้' ]
 ];
